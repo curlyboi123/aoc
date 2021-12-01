944c70d08f58cf3645e2f9d84816611366d00127
John <john.colton1@nhs.net>
2021-12-01 16:16:11 +0000

    Improved 2021 day 1 part 1 and file loader

--- 2021/01/main.go
+++ 2021/01/main.go
@@ -1,31 +1,32 @@
 package main
 
-import "fmt"
-import "io/ioutil"
-import "strings"
-import "strconv"
+import (
+	"fmt"
+    "strconv"
+	"bufio"
+	"os"
+)
 
 func main() {
-    filename := "depth_measurements_long.txt"
+	var readings []int
 
-    // Get slice of expense report
-    b, err := ioutil.ReadFile(filename)
-    if err != nil {
-        fmt.Print(err)
-    }
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		readings = append(readings, num)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 
-    filename_slice := strings.Split(string(b), "\n")
-
-	num_of_increases := 0
-
-	for i := 1; i < len(filename_slice); i++ {
-		current_value, _ := strconv.Atoi(filename_slice[i])
-		previous_value, _ := strconv.Atoi(filename_slice[i - 1])
-
-		if current_value > previous_value {
-			num_of_increases += 1
+	// Part 1
+	single_value_increases := 0
+	for i := 1; i < len(readings); i++ {
+		if readings[i] > readings[i - 1] {
+			single_value_increases += 1
 		}
 	}
+	fmt.Println("(Part 1) Single value increases", single_value_increases)
 
 	fmt.Println(num_of_increases)
 }
