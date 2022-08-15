215d026bd2e41a3a4ec3eb010a13ac6115feef4c
John Colton <jcolton96@gmail.com>
2022-08-15 13:28:49 +0100

    2021 day 4 part 2 solution

--- 2021/04/python/main.py
+++ 2021/04/python/main.py
@@ -1,54 +1,5 @@
-from re import L
 import sys
 import itertools
-
-
-def part_one(numbers_drawn: list, grids: list) -> int:
-    game_has_winner = False
-    winner_index = None
-
-    for number_drawn in numbers_drawn:
-        print(f"Number drawn is {number_drawn}")
-        for grid in grids:
-            for line in grid.lines:
-                for num in line:
-                    if number_drawn == num.val:
-                        num.is_marked = True
-        # Check for completed row or column
-        for idx, grid in enumerate(grids):
-            if check_for_completed_row(grid.lines) or check_for_completed_column(grid.lines):
-                winner_index = idx
-                game_has_winner = True
-                break
-        if game_has_winner == True:
-            winner_unmarked_numbers_sum = get_unmarked_numbers_sum(grids[winner_index])
-            return winner_unmarked_numbers_sum * number_drawn
-
-
-
-def part_two(numbers_drawn: list, grids: list) -> int:
-    game_has_winner = False
-    winner_index = None
-
-    for number_drawn in numbers_drawn:
-        print(f"Number drawn is {number_drawn}")
-        for grid in grids:
-            grid.check_is_completed()
-            if grid.is_complete == True: 
-                continue
-            for line in grid.lines:
-                for num in line:
-                    if number_drawn == num.val:
-                        num.is_marked = True
-        # Check for completed row or column
-        for idx, grid in enumerate(grids):
-            if check_for_completed_row(grid.lines) or check_for_completed_column(grid.lines):
-                winner_index = idx
-                game_has_winner = True
-                break
-        if game_has_winner == True:
-            winner_unmarked_numbers_sum = get_unmarked_numbers_sum(grids[winner_index])
-            return winner_unmarked_numbers_sum * number_drawn
 
 
 class Num:
@@ -56,64 +7,87 @@ class Num:
         self.val = val
         self.is_marked = False
 
+    def update_is_marked(self, num_to_check: int):
+        if self.val == num_to_check:
+            self.is_marked = True
 
-class Grid:
-    def __init__(self, lines: list) -> None:
-        self.lines = lines
+
+class Line:
+    def __init__(self, nums) -> None:
+        self.nums = [Num(int(num)) for num in nums]
         self.is_complete = False
-        self.has_completed_row = False
-        self.has_completed_col = False
 
-
-    def check_is_completed(self):
-        marked_row_count = 0
-        for line in self.lines:
-            pick_count = 0
-            for num in line:
-                if num.is_marked == True:
-                    pick_count += 1
-            if pick_count == len(line):
-                marked_row_count += 1
-        marked_col_count = 0
-        for i in range(0, len(self.lines)):
-            pick_count = 0
-            for line in self.lines:
-                if line[i].is_marked == True:
-                    pick_count += 1
-            if pick_count == len(line):
-                marked_col_count += 1
-
-        if marked_row_count == 5 and marked_col_count == 5:
+    def update_line_completed(self) -> None:
+        if len([num for num in self.nums if num.is_marked]) == len(self.nums):
             self.is_complete = True
 
 
+class Grid:
+    def __init__(self, lines: list) -> None:
+        self.rows = [Line(line) for line in lines]
+        self.columns = [Line([line[i] for line in lines]) for i in range(0, len(lines))]
+        self.is_complete = False
 
-def check_for_completed_row(grid: list) -> bool:
-    for line in grid:
-        pick_count = 0
-        for num in line:
-            if num.is_marked == True:
-                pick_count += 1
-        if pick_count == len(line):
-            return True
-    return False
+    def update_grid_completed(self) -> None:
+        if any([line.is_complete for line in self.rows]) or any([line.is_complete for line in self.columns]):
+            self.is_complete = True
 
 
-def check_for_completed_column(grid: list) -> bool: 
-    for i in range(0, len(grid)):
-        pick_count = 0
-        for line in grid:
-            if line[i].is_marked == True:
-                pick_count += 1
-        if pick_count == len(line):
-            return True
-    return False
-        
+def part_one(grids: list, numbers_drawn: list) -> int:
+    for num_drawn in numbers_drawn:
+        for idx, grid in enumerate(grids):
+            # Check for completed line in rows
+            for line in grid.rows:
+                if line.is_complete:
+                    continue
+                for num in line.nums:
+                    if num.is_marked:
+                        continue
+                    num.update_is_marked(int(num_drawn))
+                line.update_line_completed()
+            # Check for completed line in columns
+            for line in grid.columns:
+                if line.is_complete:
+                    continue
+                for num in line.nums:
+                    if num.is_marked:
+                        continue
+                    num.update_is_marked(int(num_drawn))
+                line.update_line_completed()
+            grid.update_grid_completed()
+            if grid.is_complete:
+                unmarked_nums = list(itertools.chain(*[[num.val for num in line.nums if not num.is_marked] for line in grid.rows]))
+                return num_drawn * sum(unmarked_nums)
 
-def get_unmarked_numbers_sum(grid: Grid) -> int:
-    unmarked_numbers = list(itertools.chain(*([[num.val for num in line if num.is_marked == False] for line in grid.lines])))
-    return sum(unmarked_numbers)
 
+def part_two(grids: list, numbers_drawn: list) -> int:
+    completed_grids = []
+    for num_drawn in numbers_drawn:
+        for idx, grid in enumerate(grids):
+            # Check for completed line in rows
+            for line in grid.rows:
+                if line.is_complete:
+                    continue
+                for num in line.nums:
+                    if num.is_marked:
+                        continue
+                    num.update_is_marked(int(num_drawn))
+                line.update_line_completed()
+            # Check for completed line in columns
+            for line in grid.columns:
+                if line.is_complete:
+                    continue
+                for num in line.nums:
+                    if num.is_marked:
+                        continue
+                    num.update_is_marked(int(num_drawn))
+                line.update_line_completed()
+            grid.update_grid_completed()
+            if grid.is_complete and grid not in completed_grids:
+                completed_grids.append(grid)
+        if len(completed_grids) == len(grids):
+            unmarked_nums = list(itertools.chain(*[[num.val for num in line.nums if not num.is_marked] for line in completed_grids[-1].rows]))
+            return num_drawn * sum(unmarked_nums)
 
 
 def main():
@@ -122,10 +96,11 @@ def main():
         lines = [line.strip() for line in f.readlines() if line.strip()]
 
     numbers_drawn = [int(num) for num in lines[0].split(",")]
-    
-    grids = [Grid([[Num(int(number)) for number in line.split()] for line in lines[x:x + 5]]) for x in range(1, len(lines[1:]), 5)]
 
-    print(f"Part One: {part_one(numbers_drawn, grids)}")
+    grids = [Grid([line.split() for line in lines[x:x + 5]]) for x in range(1, len(lines[1:]), 5)]
+
+    print(f"Part One: {part_one(grids, numbers_drawn)}")
+    print(f"Part Two: {part_two(grids, numbers_drawn)}")
 
 
 if __name__ == "__main__":
