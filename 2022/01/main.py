b8e9dc9f0876caee290e71c8a323c149237707ce
John Colton <jcolton96@gmail.com>
2022-12-02 10:02:59 +0000

    Start of 2022 challenges

---
+++ 2022/01/main.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def main():
+    path_to_file = sys.argv[1]
+    with open(path_to_file) as f:
+        content = f.read()
+
+    calories = [list(map(int, a.splitlines())) for a in content.split("\n\n")]
+    calorie_totals = [sum(elf) for elf in calories]
+    highest_calories = max(calorie_totals)
+
+    top_three_highest_calories = sorted(calorie_totals)[len(calorie_totals) - 3:]
+    
+
+    print(highest_calories)
+    print(sum(top_three_highest_calories))
+
+
+if __name__ == "__main__":
+    main()
